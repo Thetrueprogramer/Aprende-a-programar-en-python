f4743ee6978549760626a5e40661b02e65151fa2
Thetrueprogramer <enespanolmrgaming@gmail.com>
2025-01-13 12:43:29 -0500

    Create Clase 2.py

---
+++ General/Clase 2.py
@@ -0,0 +1,9 @@
+# Hoy aprenderas sobre las funciones
+# Las funciones en python son programas que podemos crear y llamar cuando queramos
+# Se crean asi: def "Nombre de la funcion" ("Parametros"): "Cambio de linea" codigo
+# Mas adelante aprenderas sobre los parametros
+def yuca():
+  yuca = 12
+
+# Tambien hay funciones predeterminadas del interprete. Miralas aqui: https://docs.python.org/es/3.13/library/functions.html
+print("Nos vemos en la proxima clase")
