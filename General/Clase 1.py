2bd08569d2bf6a46caf552b71b98aba3ec5b5ddc
Thetrueprogramer <enespanolmrgaming@gmail.com>
2025-01-13 12:24:08 -0500

    Clase 1.py

--- General/Clase 1.py
+++ General/Clase 1.py
@@ -1,1 +1,9 @@
-
+# Hoy te voy a enseñar que son las variables
+# Las variables son algo que guarda una informacion
+# Para crear una variable escribimos: "Nombre de la variable" = "Contenido". Por ejemplo
+color = 1
+# Para escribir texto usaremos las comillas (Ya sean sencillas o dobles) 
+nombre = 'Pepe'
+# Para cambiar el contenido de una variable escribirmos igual que para crearla
+color = "Blanco"
+# Te espero en la proxima clase 
