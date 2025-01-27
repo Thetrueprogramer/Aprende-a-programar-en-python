57d3fe726245fcd23a4fab08574f14ba1fdac97c
Thetrueprogramer <enespanolmrgaming@gmail.com>
2025-01-27 13:18:02 -0500

    s

--- General/Clase 1.py
+++ General/Clase 1.py
@@ -2,8 +2,8 @@
 # Las variables son algo que guarda una informacion
 # Para crear una variable escribimos: "Nombre de la variable" = "Contenido". Por ejemplo
 color = 1
-# Para escribir texto usaremos las comillas (Ya sean sencillas o dobles) 
+# Para escribir texto usaremos las comillas (Ya sean sencillas ('') o dobles ("")) 
 nombre = 'Pepe'
 # Para cambiar el contenido de una variable escribirmos igual que para crearla
-color = "Blanco"
+color = 'Blanco'
 # Te espero en la proxima clase 
