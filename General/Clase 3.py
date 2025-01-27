477d2ea5be5f355df32533f964797752ea243aaf
Thetrueprogramer <enespanolmrgaming@gmail.com>
2025-01-27 11:19:32 -0500

    Clase 3.py

--- General/Clase 3.py
+++ General/Clase 3.py
@@ -1,2 +1,13 @@
 # Bienvenido a la 3ra clase del curso de python hoy aprenderemos sobre los condicionales
 # Te enseñare con un ejemplo
+
+# Pedir al usuario que ingrese un número
+numero = float(input("Ingresa un número: "))
+
+# Usar condicionales para determinar si el número es positivo, negativo o cero
+if numero > 0:
+    print("El número es positivo.")
+elif numero < 0:
+    print("El número es negativo.")
+else:
+    print("El número es cero.")
