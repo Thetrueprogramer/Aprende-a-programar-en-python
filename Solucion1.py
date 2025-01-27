12aa845471eee178090c102362f3d8d60e0b5297
Thetrueprogramer <enespanolmrgaming@gmail.com>
2025-01-27 12:24:44 -0500

    s

--- Solucion1.py
+++ Solucion1.py
@@ -1,3 +1,6 @@
-dias = int(input('Introduce cuantos dias'))
-horas = dias * 24
-print(dias , 'dias son' , horas , 'horas')
+def Maquina():
+    dias = int(input('Introduce cuantos dias'))
+    horas = dias * 24
+    print(dias , 'dias son' , horas , 'horas')
+
+Maquina()
