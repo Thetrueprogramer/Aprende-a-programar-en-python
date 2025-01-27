e05e73ec6291ddd7b8fab94659d8de30c59a2f43
Thetrueprogramer <enespanolmrgaming@gmail.com>
2025-01-27 12:17:58 -0500

    s

--- General/Practica1.py
+++ General/Practica1.py
@@ -1,3 +1,4 @@
 # Hoy usaran todo lo aprendido
 # Van a crear un programa que diga cuantas horas tiene la cantidad de dias que introduzca el usuario
 # El formato de las operaciones necesarias son: a + b a * b
+# Al momento de poner el input() pongan int(input())
