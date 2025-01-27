05ef2ee62cd7e31f7a8b8c775787cc5f96bd3eaa
Thetrueprogramer <enespanolmrgaming@gmail.com>
2025-01-27 12:13:43 -0500

    s

---
+++ General/Practica.py
@@ -0,0 +1,3 @@
+# Hoy usaran todo lo aprendido
+# Van a crear un programa que diga cuantas horas tiene la cantidad de dias que introduzca el usuario
+# El formato de las operaciones necesarias son: a + b a * b
